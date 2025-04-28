30e1ddf4c12393b1478ba292baef2d9a2bff95c3
Ethan Lin <134337760+elin38@users.noreply.github.com>
2025-04-28 14:04:43 -0700

    preliminary functionality for user placement

--- src/phaser/Preload.ts
+++ src/phaser/Preload.ts
@@ -29,7 +29,5 @@ export class Preload extends Phaser.Scene {
         for (const tile of database.Tilemap_Packed_Tileset) {
             this.tileDictionary[tile.TileID] = tile.Description;
         }
-
-        console.log('Tile Dictionary:', this.tileDictionary);
     }
 }
